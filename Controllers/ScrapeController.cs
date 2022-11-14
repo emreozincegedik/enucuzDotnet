cad6fed3017fc7da4904d1eed93d28860c9b4875
Emre <emreozincegedik@gmail.com>
2022-11-14 21:18:06 +0300

    test better ui

--- Controllers/ScrapeController.cs
+++ Controllers/ScrapeController.cs
@@ -19,59 +19,60 @@ public class ScrapeController : ControllerBase
     {
         queryObj.website = queryObj.website.Distinct().ToList();
         bool test = true;
-        if (test)
+        Amazon t = new Amazon(queryObj.query);
+
+
+        if (queryObj.website.Count == 1 && queryObj.website[0] == null)
         {
-            return new Amazon(queryObj.query).scrape();
+            queryObj.website = new List<string>();
+        }
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        // Thread.Sleep(10000);
+
+        var result = new List<ScrapeModel>();
+        DataTable res = SQLQueries.select(queryObj);
+        if (res != null && res.Rows.Count > 0)
+        {
+            result = res.AsEnumerable().Select(row => new ScrapeModel
+            {
+                website = row.Field<string>("website"),
+                url = row.Field<string>("url"),
+                name = row.Field<string>("name"),
+                image = row.Field<string>("image"),
+                price = row.Field<Double>("price")
+
+            }).ToList();
         }
         else
         {
-
-
-            if (queryObj.website.Count == 1 && queryObj.website[0] == null)
+            if (test)
             {
-                queryObj.website = new List<string>();
-            }
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            // Thread.Sleep(10000);
-
-            var result = new List<ScrapeModel>();
-            DataTable res = SQLQueries.select(queryObj);
-            if (res != null && res.Rows.Count > 0)
-            {
-                result = res.AsEnumerable().Select(row => new ScrapeModel
-                {
-                    website = row.Field<string>("website"),
-                    url = row.Field<string>("url"),
-                    name = row.Field<string>("name"),
-                    image = row.Field<string>("image"),
-                    price = row.Field<Double>("price")
-
-                }).ToList();
+                result = t.scrape();
             }
             else
             {
                 result = new ScraperMain(queryObj).scrape();
             }
-
-            queryObj.priceHigh = queryObj.priceHigh == 0 ? Double.MaxValue : queryObj.priceHigh;
-
-            result = result.OrderBy(x => x.price).Where(x => (x.price >= queryObj.priceLow && x.price <= queryObj.priceHigh)).ToList();
-
-            if (queryObj.page != 0 && queryObj.elementInPage >= 10)
-            {
-                result = result.Skip((queryObj.page - 1) * queryObj.elementInPage).Take(queryObj.elementInPage).ToList();
-            }
-            stopWatch.Stop();
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopWatch.Elapsed;
-
-            // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
-            return result;
         }
+
+        queryObj.priceHigh = queryObj.priceHigh == 0 ? Double.MaxValue : queryObj.priceHigh;
+
+        result = result.OrderBy(x => x.price).Where(x => (x.price >= queryObj.priceLow && x.price <= queryObj.priceHigh)).ToList();
+
+        if (queryObj.page != 0 && queryObj.elementInPage >= 10)
+        {
+            result = result.Skip((queryObj.page - 1) * queryObj.elementInPage).Take(queryObj.elementInPage).ToList();
+        }
+        stopWatch.Stop();
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+        return result;
     }
 }
